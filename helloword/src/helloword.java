902ea98f00f47a220629452b07007a3dd8ec9735
M Hassan Ashraf <146906197+mhassanashraf812@users.noreply.github.com>
2025-10-23 12:34:11 +0500

    Add greeting for hassan branch

--- helloword/src/helloword.java
+++ helloword/src/helloword.java
@@ -1,10 +1,13 @@
-
+to
 import java.util.Scanner;
 
 public class helloword {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");  
+		 
+		System.out.println("hassan branch ");  
+		
 	    }
 	}
 
